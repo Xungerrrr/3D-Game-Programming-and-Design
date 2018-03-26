3eb3e117beef77443f4d4488beadc852b3a8d362
黎汛言 <37362208+Xungerrrr@users.noreply.github.com>
2018-03-26 20:43:12 +0800

    add Tic-Tac-Toe

---
+++ Homework 1/2. Tic-Tac-Toe/Assets/TicTacToe.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TicTacToe : MonoBehaviour {
+
+	private int[,] pad = new int[3, 3]; //棋盘数据
+	private int turn = 1;
+
+	private void reset () {
+		for (int i = 0; i < 3; i++) {
+			for (int j = 0; j < 3; j++) {
+				pad [i, j] = 0;
+			}
+		}
+	}
+
+	private int check() {
+		for (int i = 0; i < 3; i++) {
+			if (pad [i, 0] == pad [i, 1] && pad [i, 1] == pad [i, 2] && pad [i, 0] != 0)
+				return pad [i, 0];
+		}
+
+		for (int i = 0; i < 3; i++) {
+			if (pad [0, i] == pad [1, i] && pad [1, i] == pad [2, i] && pad [0, i] != 0)
+				return pad [0, i];
+		}
+			
+		if (pad [1, 1] != 0 && (pad [0, 0] == pad [1, 1] && pad [1, 1] == pad [2, 2]) || (pad [0, 2] == pad [1, 1] && pad [1, 1] == pad [2, 0]))
+				return pad [1, 1];
+		for (int i = 0; i < 3; i++) {
+			for (int j = 0; j < 3; j++) {
+				if (pad [i, j] == 0)
+					return 0;
+			}
+		}
+		return 2;
+	}
+
+	// Use this for initialization
+	void Start () {
+		reset ();
+	}
+
+	void OnGUI() {
+		if (GUI.Button(new Rect(400, 300, 150, 50), "Reset"))
+			reset ();
+		int result = check ();
+		if (result == 1) {
+			GUI.Label (new Rect (450, 350, 100, 50), "O wins!");
+			GUI.Label (new Rect (410, 365, 150, 50), "Please reset the game.");
+		} else if (result == -1) {
+			GUI.Label (new Rect (450, 350, 100, 50), "X wins!");
+			GUI.Label (new Rect (410, 365, 150, 50), "Please reset the game.");
+		} else if (result == 2) {  
+			GUI.Label (new Rect (450, 350, 100, 50), "Game Over!");
+			GUI.Label (new Rect (410, 365, 150, 50), "Please reset the game.");
+		}
+		for (int i = 0; i < 3; i++) {  
+			for (int j = 0; j < 3; j++) {  
+				if (pad [i, j] == 1)  
+					GUI.Button(new Rect(400 + i * 50, 100 + j * 50,50,50), "O");
+				if (pad [i, j] == -1)
+					GUI.Button(new Rect(400 + i * 50, 100 + j * 50,50,50), "X");
+				if (GUI.Button(new Rect(400 + i * 50, 100 + j * 50,50,50), "")) {
+					if (result == 0) {
+						pad [i, j] = turn;
+						turn = -turn;
+					}
+				}
+			}
+		}
+	}
+}
