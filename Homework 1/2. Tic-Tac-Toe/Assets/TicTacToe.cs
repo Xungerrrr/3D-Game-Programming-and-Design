1e2119a683956addb61763d044fda6f70f4673b4
黎汛言 <37362208+Xungerrrr@users.noreply.github.com>
2018-03-27 20:25:07 +0800

    add HW1

--- Homework 1/2. Tic-Tac-Toe/Assets/TicTacToe.cs
+++ Homework 1/2. Tic-Tac-Toe/Assets/TicTacToe.cs
@@ -4,9 +4,10 @@ using UnityEngine;
 
 public class TicTacToe : MonoBehaviour {
 
-	private int[,] pad = new int[3, 3]; //棋盘数据
-	private int turn = 1;
+	private int[,] pad = new int[3, 3]; // 棋盘数据
+	private int turn = 1; // 游戏回合
 
+	// 重置
 	private void reset () {
 		for (int i = 0; i < 3; i++) {
 			for (int j = 0; j < 3; j++) {
@@ -15,19 +16,25 @@ public class TicTacToe : MonoBehaviour {
 		}
 	}
 
+	// 检查游戏是否结束
 	private int check() {
+		// 检查纵向
 		for (int i = 0; i < 3; i++) {
 			if (pad [i, 0] == pad [i, 1] && pad [i, 1] == pad [i, 2] && pad [i, 0] != 0)
 				return pad [i, 0];
 		}
 
+		// 检查横向
 		for (int i = 0; i < 3; i++) {
 			if (pad [0, i] == pad [1, i] && pad [1, i] == pad [2, i] && pad [0, i] != 0)
 				return pad [0, i];
 		}
-			
+		
+		// 检查对角线
 		if (pad [1, 1] != 0 && (pad [0, 0] == pad [1, 1] && pad [1, 1] == pad [2, 2]) || (pad [0, 2] == pad [1, 1] && pad [1, 1] == pad [2, 0]))
 				return pad [1, 1];
+
+		// 判断游戏是否为平局
 		for (int i = 0; i < 3; i++) {
 			for (int j = 0; j < 3; j++) {
 				if (pad [i, j] == 0)
@@ -56,6 +63,7 @@ public class TicTacToe : MonoBehaviour {
 			GUI.Label (new Rect (450, 350, 100, 50), "Game Over!");
 			GUI.Label (new Rect (410, 365, 150, 50), "Please reset the game.");
 		}
+		// 绘制棋盘
 		for (int i = 0; i < 3; i++) {  
 			for (int j = 0; j < 3; j++) {  
 				if (pad [i, j] == 1)  
