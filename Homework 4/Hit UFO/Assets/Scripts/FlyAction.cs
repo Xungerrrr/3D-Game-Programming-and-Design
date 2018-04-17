1c71c39fbc4526d7424b1d50704aa2e2aa965d8b
Xungerrrr <hellolixy@icloud.com>
2018-04-17 21:33:07 +0800

    add HW4

---
+++ Homework 4/Hit UFO/Assets/Scripts/FlyAction.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlyAction : Action {
+    private float acceleration = -9.8f;                 // 飞碟的加速度
+    private float time;                                 // 过去的时间
+    private Vector3 startDirection;                     // 初速度方向
+    private Vector3 currentAngle = Vector3.zero;        // 当前的角度
+    private Vector3 accelerateDirection = Vector3.zero; // 加速度的方向
+
+
+    public static FlyAction GetAction(Vector3 direction, float angle, float power) {
+        // 初始化飞碟的初速度方向
+        FlyAction action = CreateInstance<FlyAction>();
+        if (direction.x == -1) {
+            action.startDirection = Quaternion.Euler(new Vector3(0, 0, -angle)) * Vector3.left * power;
+        }
+        else {
+            action.startDirection = Quaternion.Euler(new Vector3(0, 0, angle)) * Vector3.right * power;
+        }
+        return action;
+    }
+
+    public override void Start() { }
+
+    public override void Update() {
+        if (Director.getInstance().currentSceneController.getGameState() == GameState.PAUSE) { } // 暂停游戏
+        else {
+            // 计算物体的向下的速度, v = at
+            time += Time.fixedDeltaTime;
+            accelerateDirection.y = acceleration * time;
+
+            // 位移模拟
+            transform.position += (startDirection + accelerateDirection) * Time.fixedDeltaTime;
+            currentAngle.z = Mathf.Atan((startDirection.y + accelerateDirection.y) / startDirection.x) * Mathf.Rad2Deg;
+            transform.eulerAngles = currentAngle;
+
+            // y 坐标小于 -20，销毁动作
+            if (this.transform.position.y < -20) {
+                this.destroy = true;
+                this.enable = false;
+                this.callback.ActionEvent(this);
+            }
+        }
+    }
+}
---
+++ Homework 4/Hit UFO/Assets/Scripts/UserGUI.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UserGUI : MonoBehaviour {
+    private UserAction action;
+    // 界面样式
+    GUIStyle title;
+    GUIStyle scoreStyle;
+    GUIStyle buttonStyle;
+
+    private void Start() {
+        action = Director.getInstance().currentSceneController as UserAction;
+        title = new GUIStyle();
+        title.fontSize = 30;
+        title.normal.textColor = Color.white;
+        scoreStyle = new GUIStyle();
+        scoreStyle.fontSize = 20;
+        scoreStyle.normal.textColor = Color.white;
+        buttonStyle = new GUIStyle("button");
+        buttonStyle.fontSize = 15;
+        buttonStyle.normal.textColor = Color.white;
+        
+    }
+    private void OnGUI() {
+        // 点击鼠标触发Hit函数
+        if (Input.GetButtonDown("Fire1") && Director.getInstance().currentSceneController.getGameState() != GameState.PAUSE) {
+            Vector3 mp = Input.mousePosition;
+            action.Hit(mp);
+        }
+        // 设置按钮的内容
+        string buttonText = "";
+        if (Director.getInstance().currentSceneController.getGameState() == GameState.START ||
+            Director.getInstance().currentSceneController.getGameState() == GameState.ROUND_START)
+            buttonText = "Pause";
+        if (Director.getInstance().currentSceneController.getGameState() == GameState.FINISH)
+            buttonText = "Restart";
+        if (Director.getInstance().currentSceneController.getGameState() == GameState.PAUSE)
+            buttonText = "Start";
+        if (Director.getInstance().currentSceneController.getGameState() == GameState.ROUND_FINISH)
+            buttonText = "Next Round";
+        
+        GUI.Label(new Rect(Screen.width / 2 - 30, Screen.width / 2 - 350, 100, 100), "Hit UFO", title);
+        GUI.Label(new Rect(Screen.width / 2 - 100, Screen.width / 2 - 280, 100, 100), 
+            "Round " + Director.getInstance().currentSceneController.GetRound().ToString(), scoreStyle);
+        GUI.Label(new Rect(Screen.width / 2 + 60, Screen.width / 2 - 280, 50, 50), 
+            "Score: " + Director.getInstance().currentSceneController.GetScore().ToString(), scoreStyle);
+        // 按下按钮的操作
+        if (GUI.Button(new Rect(Screen.width / 2 - 30, Screen.width / 2 - 210, 100, 50), buttonText, buttonStyle)) {
+            if (buttonText == "Pause") {
+                Director.getInstance().currentSceneController.Pause();
+            } else if (buttonText == "Start") {
+                Director.getInstance().currentSceneController.Begin();
+            } else if (buttonText == "Next Round") {
+                Director.getInstance().currentSceneController.NextRound();
+            } else if (buttonText == "Restart") {
+                Director.getInstance().currentSceneController.Restart();
+            }
+        }
+        if (Director.getInstance().currentSceneController.getGameState() == GameState.ROUND_START) {
+
+        }
+    }
+}
