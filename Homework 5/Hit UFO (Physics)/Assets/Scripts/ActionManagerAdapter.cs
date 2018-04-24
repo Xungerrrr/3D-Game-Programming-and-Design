d4e860fa9451c7efdfe9eb8998ec5ee6845eff19
Xungerrrr <hellolixy@icloud.com>
2018-04-24 22:50:52 +0800

    add HW5

---
+++ Homework 5/Hit UFO (Physics)/Assets/Scripts/ActionManagerAdapter.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ActionManagerAdapter : MonoBehaviour, IActionManager {
+
+    // 要适配的类
+    public FlyActionManager flyActionManager;
+    public PhysicsActionManager physicsActionManager;
+
+    public void Start() {
+        flyActionManager = (FlyActionManager)gameObject.AddComponent<FlyActionManager>();
+        physicsActionManager = (PhysicsActionManager)gameObject.AddComponent<PhysicsActionManager>();
+    }
+
+    // 实现通用接口
+    public void Fly(GameObject disk, float angle, float power, bool physics) {
+        if (physics) {
+            physicsActionManager.Fly(disk, angle, power);
+        }
+        else {
+            flyActionManager.Fly(disk, angle, power);
+        }
+    }
+}
---
+++ Homework 5/Hit UFO (Physics)/Assets/Scripts/ScoreRecorder.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScoreRecorder : MonoBehaviour {
+
+    public int score; // 分数
+
+    private Dictionary<Color, int> scoreTable = new Dictionary<Color, int>(); // 用字典来记录不同飞碟对应的分数
+
+    void Start() {
+        score = 0;
+        scoreTable.Add(Color.yellow, 1);
+        scoreTable.Add(Color.red, 2);
+        scoreTable.Add(Color.white, 4);
+    }
+
+    // 记录分数的增加
+    public void Record(GameObject disk) {
+        score += scoreTable[disk.GetComponent<DiskData>().color];
+    }
+
+    public void Reset() {
+        score = 0;
+    }
+}
